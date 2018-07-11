adde73c39984ae6e587a34cefa245c436f0c3141
digidi100 <trno@live.nl>
2018-07-11 15:50:29 +0200

    removed ignored files

--- build/classes/transceptor/technology/Connection.rs
+++
@@ -1,6 +0,0 @@
-transceptor.technology.CompletionStore
-transceptor.technology.Connection$1
-transceptor.technology.Connection
-transceptor.technology.Connection$2
-transceptor.technology.Connection$3
-transceptor.technology.Connection$4
